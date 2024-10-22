2f9a8bca876c90e800d6057b0724325bf73b5da6
Mahad <mahad@xconn.io>
2024-10-22 19:53:57 +0500

    implement WAMP session close

--- src/main/kotlin/io/xconn/xconn/Session.kt
+++ src/main/kotlin/io/xconn/xconn/Session.kt
@@ -2,17 +2,24 @@ package io.xconn.xconn
 
 import io.xconn.wampproto.Session
 import io.xconn.wampproto.SessionScopeIDGenerator
+import io.xconn.wampproto.messages.Goodbye
 import io.xconn.wampproto.messages.Message
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
 import kotlin.coroutines.cancellation.CancellationException
 
 class Session(private val baseSession: BaseSession) {
     private val coroutineScope = CoroutineScope(Dispatchers.Default + Job())
     private val wampSession: Session = Session(baseSession.serializer())
     private var idGen: SessionScopeIDGenerator = SessionScopeIDGenerator()
+
+    private val goodbyeRequest: CompletableDeferred<Unit> = CompletableDeferred()
 
     init {
         coroutineScope.launch {
@@ -34,7 +41,31 @@ class Session(private val baseSession: BaseSession) {
     private val nextID: Long
         get() = idGen.next()
 
-    suspend fun close() {}
+    suspend fun close() {
+        val goodbyeMsg = Goodbye(mapOf(), closeCloseRealm)
+        val data = wampSession.sendMessage(goodbyeMsg)
+        baseSession.send(data)
+        coroutineScope.cancel()
 
-    private suspend fun processIncomingMessage(message: Message) {}
+        try {
+            withTimeout(10_000L) {
+                goodbyeRequest.await()
+            }
+        } catch (e: TimeoutCancellationException) {
+            baseSession.close()
+        } finally {
+            baseSession.close()
+        }
+    }
+
+    private suspend fun processIncomingMessage(message: Message) {
+        when (message) {
+            is Goodbye -> {
+                goodbyeRequest.complete(Unit)
+            }
+            else -> {
+                throw ProtocolError("Unexpected message type ${message.javaClass.name}")
+            }
+        }
+    }
 }
